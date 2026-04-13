1259a9de89577438fdc55df0bc1d5052c3bfca7d
shar <faethychan@gmail.com>
2026-04-13 14:40:27 +0800

    feat: add logout

--- frontend/src/features/navbar/components/exit-account.tsx
+++ frontend/src/features/navbar/components/exit-account.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@mui/material";
+import { supabase } from "lib/database/supabase";
 
 function ExitAccount() {
+    async function logout() {
+        await supabase.auth.signOut();
+    }
+
     return (
         <Button variant="outlined" onClick={logout}>
             Logout
