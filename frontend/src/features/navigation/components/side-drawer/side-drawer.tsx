692aacf36de9ae273f65bea361a1b4c2c9203799
shar <faethychan@gmail.com>
2026-04-20 16:21:24 +0800

    refactor: put drawer side bar into own file

--- frontend/src/features/navigation/components/side-drawer/side-drawer.tsx
+++ frontend/src/features/navigation/components/side-drawer/side-drawer.tsx
@@ -1,54 +1,15 @@
-import AddIcon from "@mui/icons-material/Add";
-import HomeFilledIcon from "@mui/icons-material/HomeFilled";
-import {
-  Box,
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
-import { Link as RouterLink } from "react-router"; //MUI has Link as well
+import { Drawer } from "@mui/material";
+import DrawerList from "./components/drawer-list";
 
 interface props {
   isOpen: boolean;
   toggleDrawer: () => void;
 }
 
-const drawerList = (
-  <Box sx={{ width: 250 }} role="presentation">
-    <List>
-      <ListItem>
-        <RouterLink to="/">
-          <ListItemButton>
-            <ListItemIcon>
-              <HomeFilledIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItemButton>
-        </RouterLink>
-      </ListItem>
-      <Divider />
-      <ListItem>
-        <RouterLink to="/add">
-          <ListItemButton>
-            <ListItemIcon>
-              <AddIcon />
-            </ListItemIcon>
-            <ListItemText primary="Add new product" />
-          </ListItemButton>
-        </RouterLink>
-      </ListItem>
-    </List>
-  </Box>
-);
-
 function SideDrawer({ isOpen, toggleDrawer }: props) {
   return (
     <Drawer open={isOpen} onClick={toggleDrawer}>
-      {drawerList}
+      <DrawerList />
     </Drawer>
   );
 }
