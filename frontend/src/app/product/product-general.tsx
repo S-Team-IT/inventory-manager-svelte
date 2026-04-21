ca5305fe4d396f631a8ad397a3921c8592f0d4cc
shar <faethychan@gmail.com>
2026-04-21 17:44:14 +0800

    feat: add button to clear filters

--- frontend/src/app/product/product-general.tsx
+++ frontend/src/app/product/product-general.tsx
@@ -1,5 +1,5 @@
-import { Typography } from "@mui/material";
-import { Container, Grid } from "@mui/system";
+import { Button, Typography } from "@mui/material";
+import { Container, Grid, Stack } from "@mui/system";
 import ProductTable from "features/product-table/product-table";
 import TransactionLog from "features/transaction-log/transaction-log";
 import { FilterContext } from "lib/context/context";
@@ -10,15 +10,25 @@ export default function ProductDashboard() {
   const [filter, setFilter] = useState<FILTER>("none");
   const [filterArg, setFilterArg] = useState("");
 
+  function handleClearFilter() {
+    setFilter("none");
+    setFilterArg("");
+  }
+
   return (
     <FilterContext value={{ filter, setFilter, filterArg, setFilterArg }}>
       <Container maxWidth={false} sx={{ marginX: 2 }}>
         <Grid container spacing={2}>
           {filter != "none" && (
             <Grid size={12}>
-              <Typography variant="h6" component="h1">
-                Filtering for: {filter} {filterArg}
-              </Typography>
+              <Stack direction="row" spacing={2}>
+                <Typography variant="h6" component="h1">
+                  Filtering for: {filter} {filterArg}
+                </Typography>
+                <Button variant="contained" onClick={handleClearFilter}>
+                  Clear
+                </Button>
+              </Stack>
             </Grid>
           )}
           <Grid size={{ sm: 12, lg: 8 }} component="section">
