dabd5df0c4f8c8db93f92733d3a1df0e8c96b9d5
shar <faethychan@gmail.com>
2026-05-04 09:29:24 +0800

    feat: insert outgoing transactions

--- frontend/src/features/add-transaction/outgoing-transaction-form.tsx
+++ frontend/src/features/add-transaction/outgoing-transaction-form.tsx
@@ -1,12 +1,52 @@
 import { TextField, Typography } from "@mui/material";
+import { SessionContext } from "lib/context/context";
+import { insertBulkTransactions } from "lib/database/transactions-api";
+import { useContext, useState } from "react";
+import type { transactionInsert } from "types/supabase";
 import AddItemElement from "./add-item-element";
+import Loading from "../../app/misc/loading";
 
 export function OutgoingTransactionForm() {
-  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+  const session = useContext(SessionContext);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    if (!session) return;
     e.preventDefault();
     const data: FormData = new FormData(e.target);
-    console.log(data);
+
+    const date: Date = new Date(Date.parse(data.get("date") as string));
+    const name: string = data.get("name") as string;
+    const remark: string = data.get("remark") as string;
+    const masters: string[] = data.getAll("master") as string[];
+    // terrifying
+    const quantities: number[] = (data.getAll("quantity") as string[]).map(
+      (quantityString) => Number(quantityString) * -1,
+    );
+
+    try {
+      setIsLoading(true);
+      const newTransactions: transactionInsert[] = [];
+      for (let i = 0; i < masters.length; i++) {
+        console.log(i);
+        newTransactions.push({
+          logger_id: session.user.id,
+          product_id: masters[i],
+          quantity_changed: quantities[i],
+        });
+      }
+      const isSuccess = await insertBulkTransactions(newTransactions);
+      if (!isSuccess) throw new Error("Bulk insertion failed");
+    } catch (e) {
+      console.error(e);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    alert("DO added :)");
   }
+
+  if (isLoading) return <Loading />;
 
   return (
     <form onSubmit={handleSubmit}>
