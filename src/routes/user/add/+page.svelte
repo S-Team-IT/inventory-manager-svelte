3f9fcf6e248732392b9b3a626f5241af7433652f
shar <faethychan@gmail.com>
2026-05-14 17:26:55 +0800

    feat: create add user form

--- src/routes/user/add/+page.svelte
+++ src/routes/user/add/+page.svelte
@@ -1,1 +1,27 @@
-<h1>add user page</h1>
+<script lang="ts">
+	let role = $state();
+</script>
+
+<form>
+	<fieldset class="fieldset w-xs rounded-box border border-base-300 bg-base-200 p-4">
+		<legend class="fieldset-legend">Add new user</legend>
+		<label class="input mb-1">
+			<span class="label">Email:</span>
+			<input type="text" placeholder="example@domain.com" />
+		</label>
+		<label>
+			<input type="radio" name="qs" class="radio" bind:group={role} value="QS" />
+			QS
+		</label>
+		<label>
+			<input type="radio" name="procurement" class="radio" bind:group={role} value="Procurement" />
+			Procurement
+		</label>
+		<label>
+			<input type="radio" name="project" class="radio" bind:group={role} value="Project" />
+			Project
+		</label>
+
+		<button type="submit" class="btn mt-1">Create</button>
+	</fieldset>
+</form>
