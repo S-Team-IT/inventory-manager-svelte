6ec61c0abe829915e55c2d683e8593addbc22d64
shar <faethychan@gmail.com>
2026-05-15 13:41:45 +0800

    refactor: make id regex conciser

--- src/lib/types/schemaTypes.ts
+++ src/lib/types/schemaTypes.ts
@@ -2,12 +2,7 @@ import * as z from 'zod';
 
 export const zString = z.string().trim();
 
-export const id = z
-	.string()
-	.min(1)
-	.regex(/^0-9/, 'Should start with a number')
-	.toLowerCase()
-	.trim();
+export const id = z.string().min(1).regex(/^\d/, 'Should start with a number').toLowerCase().trim();
 export const serial = z.coerce.number().int().gt(0);
 export const email = z.email().trim().toLowerCase();
 export const password = z.string().trim().min(8, 'Min. 8 characters');
