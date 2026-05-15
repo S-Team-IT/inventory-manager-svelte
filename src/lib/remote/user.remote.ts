68c5be4fcaadd796bc0f1649799fa7efdd2fc5b6
shar <faethychan@gmail.com>
2026-05-15 09:53:57 +0800

    feat: add role to create user remote form

--- src/lib/remote/user.remote.ts
+++ src/lib/remote/user.remote.ts
@@ -1,15 +1,19 @@
 import { form } from '$app/server';
 import { handleQueryErrors, sql } from '$lib/server/postgres';
-import { email, password } from '$lib/types/schemaTypes';
+import { email, password, role } from '$lib/types/schemaTypes';
 import { hashPassword } from '$lib/utils/hash';
 import z from 'zod';
 
-export const createUser = form(z.object({ email, password }), async ({ email, password }) => {
-	try {
-		const passwordHash = await hashPassword(password);
-		const result = await sql`INSERT INTO users (email, password) VALUES(${email}, ${passwordHash})`;
-		if (result.count !== 1) throw new Error('Failed to create user');
-	} catch (e) {
-		handleQueryErrors(e);
+export const createUser = form(
+	z.object({ email, password, role }),
+	async ({ email, password, role }) => {
+		try {
+			const passwordHash = await hashPassword(password);
+			const result =
+				await sql`INSERT INTO users (email, password_hash, role) VALUES(${email}, ${passwordHash}, ${role})`;
+			if (result.count !== 1) throw new Error('Failed to create user');
+		} catch (e) {
+			handleQueryErrors(e);
+		}
 	}
-});
+);
