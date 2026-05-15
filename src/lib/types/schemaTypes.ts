3db1757a1e4841220c65f79943ea6d1ca2752c32
shar <faethychan@gmail.com>
2026-05-15 09:53:37 +0800

    feat: add schema for user role

--- src/lib/types/schemaTypes.ts
+++ src/lib/types/schemaTypes.ts
@@ -11,3 +11,9 @@ export const id = z
 export const serial = z.coerce.number().int().gt(0);
 export const email = z.email().trim().toLowerCase();
 export const password = z.string().trim().min(8, 'Min. 8 characters');
+export const role = z
+	.string()
+	.refine((str) => str === 'QS' || str === 'Procurement' || str === 'Project', {
+		error: 'Role does not match any existing roles',
+		path: ['role']
+	});
