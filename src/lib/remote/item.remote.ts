c6d19fdbaa8fdb02cb8e03ad189f2c6ba84945c6
shar <faethychan@gmail.com>
2026-05-18 10:24:13 +0800

    feat: update getItems query and decouple from type Item

--- src/lib/remote/item.remote.ts
+++ src/lib/remote/item.remote.ts
@@ -1,11 +1,21 @@
 import { query } from '$app/server';
-import type { Item } from '$lib/types/databaseTypes';
 import { handleQueryErrors } from '$lib/utils/errorHandling';
 import { sql } from '$lib/server/postgres';
 
 export const getItems = query(async () => {
 	try {
-		return await sql<Item[]>`SELECT * FROM items`;
+		return await sql`SELECT
+      id,
+      name,
+      category_id AS "categoryID",
+      supplier_id AS "supplierID",
+      thumbnail,
+      photos
+      FROM items`;
+	} catch (e) {
+		handleQueryErrors(e);
+	}
+});
 	} catch (e) {
 		handleQueryErrors(e);
 	}
