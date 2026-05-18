3e72e65f1d37ba6ec4de3c98e30e80509aa83db7
shar <faethychan@gmail.com>
2026-05-18 10:24:29 +0800

    feat: create query getItemsFullInfo

--- src/lib/remote/item.remote.ts
+++ src/lib/remote/item.remote.ts
@@ -16,6 +16,21 @@ export const getItems = query(async () => {
 		handleQueryErrors(e);
 	}
 });
+
+export const getItemsFullInfo = query(async () => {
+	try {
+		return await sql`SELECT
+       i.id,
+       i.name,
+       c.name AS "category",
+       i.category_id AS "categoryID",
+       s.name AS "supplier",
+       i.supplier_id AS "supplierID",
+       i.thumbnail,
+       i.photos
+       FROM items i
+       JOIN categories c ON i.category_id = c.id
+       JOIN suppliers s ON i.supplier_id = s.id`;
 	} catch (e) {
 		handleQueryErrors(e);
 	}
