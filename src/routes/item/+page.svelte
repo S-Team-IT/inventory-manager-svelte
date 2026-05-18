88ad9a3a35d837e5ba97eabd3f9ca4762883a326
shar <faethychan@gmail.com>
2026-05-18 10:26:17 +0800

    feat: display items in table

--- src/routes/item/+page.svelte
+++ src/routes/item/+page.svelte
@@ -4,7 +4,7 @@
 
 <h1>hello</h1>
 
-<!-- <table class="table max-w-200">
+<table class="table max-w-200">
 	<thead>
 		<tr>
 			<th class="w-25"></th>
@@ -15,14 +15,13 @@
 		</tr>
 	</thead>
 	<tbody>
-		{#each data.items as { id, masterNumber, name, category, quantity, thumbnailUrl, photoUrls } (id)}
+		{#each data.items as { id, name, category, thumbnail } (id)}
 			<tr class="hover:bg-base-300">
-				<th class="w-25 text-2xl">{masterNumber}</th>
-				<th class="flex w-50 items-center justify-center"><img src={thumbnailUrl} alt="" /></th>
+				<th class="w-25 text-2xl">{id}</th>
+				<th class="flex w-50 items-center justify-center"><img src={thumbnail} alt="" /></th>
 				<th>{name}</th>
 				<th>{category}</th>
-				<th class="text-center">{quantity}</th>
 			</tr>
 		{/each}
 	</tbody>
-</table> -->
+</table>
