f5922174f2b7f3c0de32242609556eedca4e7722
shar <faethychan@gmail.com>
2026-05-19 15:17:40 +0800

    feat: create Combobox component

---
+++ src/lib/components/combobox.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import type { Generic } from '$lib/types/databaseTypes';
+	import InputIssues from './inputIssues.svelte';
+
+	const { label, field, list, subtitle = '' } = $props();
+
+	let inputValue = $derived.by(() => {
+		if (!field.value()) return '';
+		const str: string = field.value();
+		return str.toLowerCase();
+	});
+	let filteredList = $derived.by(() =>
+		(list as Generic[]).filter(({ name }) => name.toLowerCase().includes(inputValue))
+	);
+</script>
+
+<div class="mb-4">
+	<label class="input w-full">
+		<span class="label">{label}:</span>
+		<input list={`${label}-list`} {...field.as('text')} />
+	</label>
+	<span class="label">{subtitle}</span>
+	<InputIssues {field} />
+</div>
+
+<datalist id={`${label}-list`}>
+	{#each filteredList as { id, name } (id)}
+		<option value={name}></option>
+	{/each}
+</datalist>
+
+<!-- <label class="input w-full">
+	<span class="label">{label}:</span>
+	<input
+		list="combobox-list"
+		value={input}
+		oninput={(e) => (input = e.currentTarget.value.replace('Add', '').replaceAll(`"`, '').trim())}
+	/>
+</label>
+<span class="label">{subtitle}</span>
+
+<datalist id="combobox-list">
+	{#each filteredList as { id, name } (id)}
+		<option value={name}></option>
+	{/each}
+	{#if filteredList?.length !== suppliers?.length}
+		<option value={`Add "${input}"`}></option>
+	{/if}
+</datalist> -->
