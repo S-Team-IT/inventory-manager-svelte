a20b5c4371c2a2136e1b4808d487772b3ef1479e
shar <faethychan@gmail.com>
2026-05-20 14:07:47 +0800

    refactor: move itemCard to components

---
+++ src/lib/components/itemCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import ImageModal from '$lib/components/imageModal.svelte';
+	import type { Item } from '$lib/types/databaseTypes';
+
+	const { masterNumber, name, category, supplier, quantity, thumbnail, photos }: Item = $props();
+</script>
+
+<div class="card bg-accent shadow-sm">
+	<ImageModal id={masterNumber} thumbnailSrc={thumbnail} gallerySrc={photos} />
+	<div class="card-body">
+		<h2 class="card-title">{name}</h2>
+		<p>
+			Category: {category} <br />
+			Supplier: {supplier} <br />
+			Quantity: {quantity}
+		</p>
+	</div>
+</div>
