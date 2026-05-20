51b8224188fd75ca10f392780cf3c8521b3cb82d
shar <faethychan@gmail.com>
2026-05-20 14:08:42 +0800

    fix: always log and prevent printing twice

--- src/lib/utils/errorHandling.ts
+++ src/lib/utils/errorHandling.ts
@@ -4,15 +4,15 @@ import type { PostgresError } from 'postgres';
 export const handleQueryErrors = (e: unknown, customPsqlHandler?: (e: PostgresError) => void) => {
 	if (isHttpError(e) || isRedirect(e)) throw e;
 	if (isPostgresError(e)) {
+		console.error(e);
 		const psqlError = e as PostgresError;
 
 		if (psqlError.code.startsWith('0800')) error(500, 'Database connection failed');
 		if (psqlError.code === '42601') throw new Error('Syntax error found in query statement');
 		if (customPsqlHandler) customPsqlHandler(psqlError);
-		console.error(e);
-		throw new Error('Unhandled psql error', { cause: e });
+		throw new Error('Unhandled psql error');
 	}
-	throw new Error('Unhandled error', { cause: e });
+	throw new Error('Unhandled error');
 };
 
 export const isPostgresError = (e: unknown): boolean => {
