b4085316053d620055f8002c007bfe6cc1531309
shar <faethychan@gmail.com>
2026-05-21 10:44:58 +0800

    feat: add breadcrumb

--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import ItemCard from '$lib/components/itemCard.svelte';
 
 	const { data } = $props();
 </script>
+
+<div class="breadcrumbs text-sm">
+	<ul>
+		<li><a href={resolve('/item')}>Item List</a></li>
+		<li>Add Item</li>
+	</ul>
+</div>
 
 {#if !data.item}
 	404 not found
