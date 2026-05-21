5db2b964a4ba9157157d6c2288ce39dd1715db16
shar <faethychan@gmail.com>
2026-05-21 15:28:38 +0800

    feat: display item info

--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import type { Item } from '$lib/types/databaseTypes.js';
 
 	const { data } = $props();
+	let { masterNumber, name, category, categoryID, supplier, supplierID, quantity }: Item = $derived(
+		data.item
+	);
 </script>
 
 <div class="breadcrumbs text-sm">
 	<ul>
-		<li><a href={resolve('/item')}>Item List</a></li>
+		<li><a href={resolve('/')} class="underline">Home</a></li>
+		<li><a href={resolve('/item')} class="underline">Item List</a></li>
 		<li>Add Item</li>
 	</ul>
 </div>
@@ -14,5 +19,15 @@
 {#if !data.item}
 	404 not found
 {:else}
-	<div class="max-w-75">item here</div>
+	<div class="max-w-75">
+		<h1>{name}</h1>
+		<h2>{masterNumber}</h2>
+		<select name="" id="">
+			<option value={categoryID}>{category}</option>
+		</select>
+		<select name="" id="">
+			<option value={supplierID}>{supplier}</option>
+		</select>
+		<p>{quantity}</p>
+	</div>
 {/if}
