2c14bb21ac9483e67282f4fc8daa12a23ea06b22
shar <faethychan@gmail.com>
2026-05-22 11:25:13 +0800

    feat: pass categories and suppliers

--- src/routes/item/[slug]/+page.server.ts
+++ src/routes/item/[slug]/+page.server.ts
@@ -1,10 +1,18 @@
+import { getCategories } from '$lib/remote/category.remote.js';
 import { getItemFullInfo } from '$lib/remote/item.remote.js';
+import { getSuppliers } from '$lib/remote/supplier.remote.js';
 
 export async function load({ params }) {
 	const item = await getItemFullInfo(params.slug);
-	if (!item) throw new Error('getItemFullInfo returned undefined');
+	if (!item) throw new Error('getItemFullInfo item undefined');
+	const categories = await getCategories();
+	if (!categories) throw new Error('getItemFullInfo categories undefined');
+	const suppliers = await getSuppliers();
+	if (!suppliers) throw new Error('getItemFullInfo suppliers undefined');
 
 	return {
-		item
+		item,
+		categories,
+		suppliers
 	};
 }
