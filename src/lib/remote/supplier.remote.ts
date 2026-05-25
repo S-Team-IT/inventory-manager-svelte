7546641b78c54c3b1d17012ffdac7ab6bdd855b0
shar <faethychan@gmail.com>
2026-05-25 16:41:10 +0800

    fix: change category for supplier

--- src/lib/remote/supplier.remote.ts
+++ src/lib/remote/supplier.remote.ts
@@ -1,6 +1,6 @@
 import { command, query } from '$app/server';
 import { sql } from '$lib/server/postgres';
-import type { Category, Supplier } from '$lib/types/databaseTypes';
+import type { Supplier } from '$lib/types/databaseTypes';
 import { zString } from '$lib/types/schemaTypes';
 import { handleQueryErrors } from '$lib/utils/errorHandling';
 
@@ -14,15 +14,15 @@ export const getSuppliers = query(async () => {
 
 export const getOrCreateSupplier = command(zString, async (name) => {
 	try {
-		const [result] = await sql<Category[]>`
+		const [result] = await sql<Supplier[]>`
 			WITH i AS(
-				INSERT INTO categories (name) VALUES (${name}) 
+				INSERT INTO suppliers (name) VALUES (${name}) 
 				ON CONFLICT(name) DO NOTHING
 				RETURNING id
 			)
 			SELECT id FROM i
 			UNION ALL
-			SELECT id FROM categories WHERE name = ${name}
+			SELECT id FROM suppliers WHERE name = ${name}
 			LIMIT 1;`;
 		return result;
 	} catch (e) {
