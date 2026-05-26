6b8ded13613e2e26a340bca4f8532cb89493b832
shar <faethychan@gmail.com>
2026-05-26 16:24:07 +0800

    feat: create transaction accordion component

---
+++ src/lib/components/transactionAccordion.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { CompleteIncomingTransaction } from '$lib/types/types';
+
+	type Props = {
+		transactions: CompleteIncomingTransaction[];
+	};
+
+	const { transactions }: Props = $props();
+</script>
+
+{#each transactions as { id, createdAt, deliveryDate, supplier, deliveryID, items } (id)}
+	<details class="collapse border border-base-300 bg-base-100" name="my-accordion-det-1" open>
+		<summary class="collapse-title font-semibold">{deliveryID} {createdAt}</summary>
+		<div class="collapse-content text-sm">
+			<ul>
+				{#each items as item, i (i)}
+					<li>{item.id} {item.name} {item.quantity}</li>
+				{/each}
+			</ul>
+		</div>
+	</details>
+{/each}
