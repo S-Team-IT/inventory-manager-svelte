2440ebc8e9df524192b40e2cdbfb04cc1220e2cd
shar <faethychan@gmail.com>
2026-05-28 15:14:10 +0800

    feat: create capitalizeFirstLetter func

--- src/lib/utils/stringTransform.ts
+++ src/lib/utils/stringTransform.ts
@@ -2,3 +2,7 @@ export function truncateString(str: string, maxLength: number): string {
 	if (str.length <= maxLength) return str;
 	return str.slice(0, maxLength - 3) + '...';
 }
+
+export function capitalizeFirstLetter(str: string): string {
+	return str[0].toUpperCase() + str.slice(1);
+}
