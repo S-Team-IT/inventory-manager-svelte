b0800fdb532f5d6b4de5a561cfba4741522050df
shar <faethychan@gmail.com>
2026-05-28 14:06:16 +0800

    feat: wrap content in flex div

--- src/routes/transaction/incoming/+page.svelte
+++ src/routes/transaction/incoming/+page.svelte
@@ -6,7 +6,7 @@
 	import TransactionAccordion from '$lib/components/transactionAccordion.svelte';
 	import { getItemNameByMaster } from '$lib/remote/item.remote.js';
 	import { createIncomingTransaction } from '$lib/remote/transaction.remote.js';
-	import type { TransactionItem } from '$lib/types/types.js';
+	import type { ItemTransaction } from '$lib/types/databaseTypes.js';
 	import { truncateString } from '$lib/utils/stringTransform.js';
 	import { toast } from 'svelte-sonner';
 
@@ -14,97 +14,101 @@
 
 	const { data } = $props();
 	let masterInput = $state<string>('');
-	let items = $state<TransactionItem[]>([]);
+	let items = $state<ItemTransaction[]>([]);
 </script>
 
-<Form
-	remoteForm={createIncomingTransaction}
-	legend="Enter Delivery Order"
-	errorMsg="Failed to submit DO"
-	successMsg="DO added"
-	onSuccess={() => {
-		items = [];
-		masterInput = '';
-	}}
->
-	<Input
-		label="Delivery Date"
-		type="date"
-		field={date}
-		value={new Date().toISOString().split('T')[0]}
-	/>
-	<Combobox
-		label="Supplier"
-		field={supplier}
-		list={data.suppliers}
-		subtitle="New suppliers can be added as needed"
-		placeholder="Enter supplier name"
-	/>
-	<Input
-		label="DO Number"
-		field={deliveryID}
-		type="text"
-		placeholder="Enter delivery order number"
-	/>
-	<div class="divider mt-0"></div>
-	<fieldset class="mb-4">
-		<label class="input mb-2 w-full pr-0">
-			<span class="label">Add items:</span>
-			<input type="text" bind:value={masterInput} placeholder="Enter master number" />
-			<button
-				aria-label="add-item"
-				class="btn rounded-s-none btn-secondary"
-				type="button"
-				onclick={async () => {
-					const result = await getItemNameByMaster(masterInput.toLowerCase().trim()).run();
-					if (!result) {
-						toast.error(`Master number ${masterInput} not found.`);
-						return;
-					}
+<div class="flex">
+	<Form
+		remoteForm={createIncomingTransaction}
+		legend="Enter Delivery Order"
+		errorMsg="Failed to submit DO"
+		successMsg="DO added"
+		onSuccess={() => {
+			items = [];
+			masterInput = '';
+		}}
+	>
+		<Input
+			label="Delivery Date"
+			type="date"
+			field={date}
+			value={new Date().toISOString().split('T')[0]}
+		/>
+		<Combobox
+			label="Supplier"
+			field={supplier}
+			list={data.suppliers}
+			subtitle="New suppliers can be added as needed"
+			placeholder="Enter supplier name"
+		/>
+		<Input
+			label="DO Number"
+			field={deliveryID}
+			type="text"
+			placeholder="Enter delivery order number"
+		/>
+		<div class="divider mt-0"></div>
+		<fieldset class="mb-4">
+			<label class="input mb-2 w-full pr-0">
+				<span class="label">Add items:</span>
+				<input type="text" bind:value={masterInput} placeholder="Enter master number" />
+				<button
+					aria-label="add-item"
+					class="btn rounded-s-none btn-secondary"
+					type="button"
+					onclick={async () => {
+						const result = await getItemNameByMaster(masterInput.toLowerCase().trim()).run();
+						if (!result) {
+							toast.error(`Master number ${masterInput} not found.`);
+							return;
+						}
 
-					const newItem = {
-						id: result.id,
-						master: masterInput,
-						name: result.name,
-						quantity: 1
-					};
-					items.push(newItem);
-				}}><span class="icon-[ic--baseline-plus]"></span></button
-			>
-		</label>
-		<InputIssues field={ids} />
-		<table class="table table-zebra">
-			<thead>
-				<tr>
-					<th class="w-15">Master</th>
-					<th>Name</th>
-					<th class="w-10">Qty</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each items as { id, master, name, quantity }, i (i)}
+						const newItem = {
+							id: result.id,
+							master: masterInput,
+							name: result.name,
+							quantity: 1
+						};
+						items.push(newItem);
+					}}><span class="icon-[ic--baseline-plus]"></span></button
+				>
+			</label>
+			<InputIssues field={ids} />
+			<table class="table table-zebra">
+				<thead>
 					<tr>
-						<th class="w-15">{master}</th>
-						<th>{truncateString(name, 20)}</th>
-						<th
-							><input
-								class="w-10 border ps-1"
-								type="number"
-								id={`input${i}`}
-								name={`input${i}`}
-								bind:value={items[i].quantity}
-								step="1"
-								min="1"
-							/>
-							<InputIssues field={quantities[i]} />
-							<input {...ids[i].as('hidden', id)} />
-							<input {...quantities[i].as('hidden', quantity)} />
-						</th>
+						<th class="w-15">Master</th>
+						<th>Name</th>
+						<th class="w-10">Qty</th>
 					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</fieldset>
-	<button class="btn btn-soft btn-primary">Add Delivery Order</button>
-</Form>
-<TransactionAccordion transactions={data.transactions} />
+				</thead>
+				<tbody>
+					{#each items as { id, master, name, quantity }, i (i)}
+						<tr>
+							<th class="w-15">{master}</th>
+							<th>{truncateString(name, 20)}</th>
+							<th
+								><input
+									class="w-10 border ps-1"
+									type="number"
+									id={`input${i}`}
+									name={`input${i}`}
+									bind:value={items[i].quantity}
+									step="1"
+									min="1"
+								/>
+								<InputIssues field={quantities[i]} />
+								<input {...ids[i].as('hidden', id)} />
+								<input {...quantities[i].as('hidden', quantity)} />
+							</th>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</fieldset>
+		<button class="btn btn-soft btn-primary">Add Delivery Order</button>
+	</Form>
+	<div>
+		<TransactionAccordion transactions={data.transactions} />
+	</div>
+</div>
