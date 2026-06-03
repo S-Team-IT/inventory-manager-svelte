d257736bbe9251710347df6dca8173f9a8691176
shar <faethychan@gmail.com>
2026-06-03 11:13:45 +0800

    feat: import type defintions

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import type { User, SessionClient } from "$lib/types/databaseTypes"
+
 declare global {
 	namespace App {
 		// interface Error {}
