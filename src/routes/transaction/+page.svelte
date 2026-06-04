010c68095a8c771eaba3ade6cdce073d58a9fe8c
shar <faethychan@gmail.com>
2026-06-04 17:00:08 +0800

    feat: display all transactions temp

---
+++ src/routes/transaction/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const { data } = $props();
+</script>
+
+{#each data.transactions as transaction, i (i)}
+	<tr>
+		{#each Object.entries(transaction) as [key, value] (key)}
+			<th>{value}</th>
+		{/each}
+	</tr>
+{/each}
