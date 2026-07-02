ec2a9fcd4a1d7a25216e6c156431a8ad064ca7c1
shar <faethychan@gmail.com>
2026-07-02 10:46:53 +0800

    feat: create number sort util

--- src/lib/utils/arraySort.ts
+++ src/lib/utils/arraySort.ts
@@ -1,3 +1,8 @@
 export function localeCompareSort(a: string, b: string) {
 	return a.localeCompare(b, 'en', { sensitivity: 'base', numeric: true });
 }
+
+export function numberSort(a: number, b: number) {
+	return a - b;
+}
+
