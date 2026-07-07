7b0292dc2146aabacc168d30b8ef84037795b272
shar <faethychan@gmail.com>
2026-07-07 17:49:19 +0800

    feat: uncomment date sort and reverse by default

--- src/routes/transaction/+page.svelte
+++ src/routes/transaction/+page.svelte
@@ -3,25 +3,25 @@
 
 	const { data } = $props();
 
-	let isTransactionsReversed = $state<boolean>(false);
+	let isTransactionsReversed = $state<boolean>(true);
 	let transactionsReversed = $derived(data.transactions.toReversed());
 	let transactionsToDisplay = $derived(
 		isTransactionsReversed ? transactionsReversed : data.transactions
 	);
 
-	// function toggleTransactionsReversed() {
-	// 	isTransactionsReversed = !isTransactionsReversed;
-	// }
+	function toggleTransactionsReversed() {
+		isTransactionsReversed = !isTransactionsReversed;
+	}
 </script>
 
 <svelte:head>
 	<title>Transactions</title>
 </svelte:head>
 
-<!-- <div class="filter-buttons-group">
+<div class="filter-buttons-group">
 	<button class="btn btn-primary" onclick={toggleTransactionsReversed}>
 		Date <span class={isTransactionsReversed ? 'icon-[mdi--arrow-up]' : 'icon-[mdi--arrow-down]'}
 		></span>
 	</button>
-</div> -->
+</div>
 <div class="m-10"><TransactionAccordionGroup transactions={transactionsToDisplay} /></div>
