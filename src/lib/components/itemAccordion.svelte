6bc236fe349fed0fb826c23f303dfdd3899823cf
shar <faethychan@gmail.com>
2026-07-07 15:03:21 +0800

    refactor: make itemAccordion use base accordion class

--- src/lib/components/itemAccordion.svelte
+++ src/lib/components/itemAccordion.svelte
@@ -4,14 +4,15 @@
 	import ImageModal from '$lib/components/imageModal.svelte';
 	import { deleteItem } from '$lib/remote/item.remote';
 	import type { DetailedItem } from '$lib/types/databaseTypes';
+	import type { SvelteComponent } from 'svelte';
 	import { toast } from 'svelte-sonner';
+	import Accordion from './base/accordion.svelte';
 
 	let {
-		id,
+		// id,
 		master,
 		name,
 		category,
-		// supplier,
 		quantity,
 		thumbnail,
 		gallery,
@@ -19,61 +20,46 @@
 		minimumQuantity
 	}: DetailedItem & { deletedItems?: string[] | undefined } = $props();
 
-	function openDeleteConfirmation() {
-		const modal = document.getElementById(`confirm-modal${master}`);
-		(modal as HTMLDialogElement).showModal();
-	}
+	let elementID = $derived(`confirm-modal${master}`);
+	let uniqueForm = $derived(deleteItem.for(elementID));
 
-	function closeDeleteConfirmation() {
-		const modal = document.getElementById(`confirm-modal${master}`);
-		(modal as HTMLDialogElement).close();
-	}
+	let accordionRef: SvelteComponent;
 </script>
 
-<details class="collapse border border-base-300 bg-base-100" name="my-accordion-det-1" open>
-	<summary class="collapse-title font-semibold">
-		<div class="flex items-center justify-between gap-10">
-			#{master} | {name}
-			<button
-				class="btn size-12.5 btn-soft btn-error"
-				aria-label="delete"
-				onclick={openDeleteConfirmation}>
-				<span class="icon-[tabler--trash]"></span>
-			</button>
-		</div>
-	</summary>
-	<div class="collapse-content text-sm">
-		<ImageModal id={master} thumbnailSrc={thumbnail} gallerySrc={gallery} /> <br />
-		{category} <br />
-		{quantity | 0} in inventory <br />
-		{minimumQuantity | 0} minimum
-	</div>
-</details>
+<Accordion
+	bind:this={accordionRef}
+	elementID={`confirm-modal${master}`}
+	{summaryStart}
+	{content}
+	{deleteForm}
+	deletePrompt={`Are you sure you want to delete ${name}?`}>
+</Accordion>
 
-<dialog id={`confirm-modal${master}`} class="modal">
-	<div class="modal-box">
-		<h2 class="text-lg">
-			Are you sure you want to delete {name}?
-		</h2>
-		<div class="modal-action">
-			<form method="dialog">
-				<button class="btn btn-soft btn-secondary">Cancel</button>
-			</form>
-			<form
-				{...deleteItem.for(id).enhance(async ({ form, submit }) => {
-					if (await submit()) {
-						form.reset();
-						toast.success('Item deleted');
-						if (deletedItems) deletedItems.push(master);
-						closeDeleteConfirmation();
-						goto(resolve('/item'));
-					} else {
-						toast.error('Failed to delete item');
-					}
-				})}>
-				<input {...deleteItem.fields.master.as('hidden', master)} />
-				<button class="btn btn-primary">Confirm</button>
-			</form>
-		</div>
-	</div>
-</dialog>
+{#snippet summaryStart()}
+	#{master} | {name}
+{/snippet}
+
+{#snippet content()}
+	<ImageModal id={master} thumbnailSrc={thumbnail} gallerySrc={gallery} /> <br />
+	{category} <br />
+	{quantity | 0} in inventory <br />
+	{minimumQuantity | 0} minimum
+{/snippet}
+
+{#snippet deleteForm()}
+	<form
+		{...uniqueForm.enhance(async ({ form, submit }) => {
+			if (await submit()) {
+				form.reset();
+				toast.success('Item deleted');
+				if (deletedItems) deletedItems.push(master);
+				accordionRef.closeDeleteConfirmation();
+				goto(resolve('/item'));
+			} else {
+				toast.error('Failed to delete item');
+			}
+		})}>
+		<input {...uniqueForm.fields.master.as('hidden', master)} />
+		<button class="btn btn-primary">Confirm</button>
+	</form>
+{/snippet}
