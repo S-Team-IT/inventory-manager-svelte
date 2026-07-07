9cbba1685967ee9e58471f570aff1f9179e9fe2a
shar <faethychan@gmail.com>
2026-07-07 18:01:59 +0800

    refactor: pass incoming and outgoing transactions separately

--- src/routes/transaction/+page.server.ts
+++ src/routes/transaction/+page.server.ts
@@ -1,18 +1,8 @@
 import { getIncomingTransactions, getOutgoingTransactions } from '$lib/remote/transaction.remote';
-import type { CompleteTransaction } from '$lib/types/databaseTypes';
-import { numberSort } from '$lib/utils/arraySort';
 
 export async function load() {
-	const incomingTransactions = await getIncomingTransactions();
-	if (!incomingTransactions) throw new Error('getIncomingTransactions returned undefined');
-	const outgoingTransactions = await getOutgoingTransactions();
-	if (!outgoingTransactions) throw new Error('getOutgoingTransactions returned undefined');
-
 	return {
-		transactions: sortByDate([...incomingTransactions, ...outgoingTransactions])
+		incomingTransactions: await getIncomingTransactions(),
+		outgoingTransactions: await getOutgoingTransactions()
 	};
-}
-
-function sortByDate(transactions: CompleteTransaction[]) {
-	return transactions.toSorted((b, a) => numberSort(b.createdAt.getTime(), a.createdAt.getTime()));
 }
