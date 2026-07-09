93ad96bea3106c218195b8651e7f5362312d11dd
shar <faethychan@gmail.com>
2026-07-09 11:32:26 +0800

    feat: uncomment sort by date

--- src/routes/transaction/+page.svelte
+++ src/routes/transaction/+page.svelte
@@ -9,19 +9,19 @@
 		isTransactionsReversed ? transactionsReversed : data.transactions
 	);
 
-	// function toggleTransactionsReversed() {
-	// 	isTransactionsReversed = !isTransactionsReversed;
-	// }
+	function toggleTransactionsReversed() {
+		isTransactionsReversed = !isTransactionsReversed;
+	}
 </script>
 
 <svelte:head>
 	<title>Transactions</title>
 </svelte:head>
 
-<!-- <div class="filter-buttons-group">
+<div class="filter-buttons-group">
 	<button class="btn btn-primary" onclick={toggleTransactionsReversed}>
 		Date <span class={isTransactionsReversed ? 'icon-[mdi--arrow-up]' : 'icon-[mdi--arrow-down]'}
 		></span>
 	</button>
-</div> -->
+</div>
 <div class="m-10"><TransactionAccordionGroup transactions={transactionsToDisplay} /></div>
